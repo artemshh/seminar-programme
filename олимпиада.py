d68ad8b447f868c1d55e3fac5e1c53fefcea3000
Филимонов Артем Сергеевич <FiAS.21@free.uni-dubna.ru>
2021-12-07 14:16:42 +0300

    https://github.com/artemshh/seminar-programme.git

--- олимпиада.py
+++ олимпиада.py
@@ -1,4 +1,6 @@
-x=int(input().split)
-a={}
-
-
+d= {"phone": "Телефон", "water": "Вода","opel vectra b": "корыто"}    
+word = input()
+trans_word=input()
+if not word in d.keys():
+    d[word] = trans_word
+print(d)
